0a2f295ef595c937bbe7ee00ea664d9c9c9a4316
Dora-ApplePie <dasha0000golenko@gmail.com>
2022-07-27 19:38:25 +0300

    create instance and loginRequest

---
+++ src/api/login-api/loginAPI.ts
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+export type ProfileType = {
+    avatar: string;
+    created: string | null;
+    email: string | null;
+    isAdmin: boolean | null;
+    name: string;
+    publicCardPacksCount: number | null;
+    rememberMe: boolean | null;
+    updated: string | null;
+    verified: boolean | null;
+    _id: string | null;
+    error?: string;
+};
+
+export const instance = axios.create({
+    baseURL:  'http://localhost:7542/2.0/',
+    // || process.env.REACT_APP_BASE_URL
+    withCredentials: true,
+})
+
+export const authApi = {
+    loginRequest(data: { email: string; password: string; rememberMe: boolean }) {
+        return instance.post<ProfileType>('auth/login', data);
+    },
+}
